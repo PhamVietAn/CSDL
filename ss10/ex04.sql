154dd024a44c13a3996eb6facf451bfe2242ff42
Pham Viet An <anphamls6969@gmail.com>
2026-01-11 22:43:35 +0700

    ss10

--- ss10/ex04.sql
+++ ss10/ex04.sql
@@ -13,10 +13,26 @@ where user_id = 1
 create index idx_created_at_user_id
 on posts (created_at, user_id);
 
--- chạy lại truy vấn sau khi tạo index
+-- truy vấn tìm user có email = 'an@gmail.com
 explain analyze
-select post_id, content, created_at
-from posts
-where user_id = 1
-  and created_at >= '2026-01-01'
-  and created_at <  '2027-01-01';
+select user_id, username, email
+from users
+where email = 'an@gmail.com';
+
+-- tạo unique index idx_email
+create unique index idx_email
+on users (email);
+
+-- truy vấn lại sau khi tạo unique index
+explain analyze
+select user_id, username, email
+from users
+where email = 'an@gmail.com';
+
+-- xóa composite index
+drop index idx_created_at_user_id
+on posts;
+
+-- xóa unique index
+drop index idx_email
+on users;
